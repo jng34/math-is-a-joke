955692575c9e9887a1c864d31030edfad1f578d2
James Ng <njames15@gmail.com>
2026-02-14 17:38:15 -0500

    updated dependencies

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,4 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
+require "logger" # Fixes missing Logger dependency in Rails 6.1
